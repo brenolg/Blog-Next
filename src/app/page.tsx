ba71efd65659fe6fe52c7fb03f5217b1fd281b6f
brenolg <brenolgar@gmail.com>
2026-04-17 10:55:07 -0300

    rotas admin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { PostFeatured } from "@/components/PostFeatured";
 import { PostsList } from "@/components/PostList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
-import { PostFeatured } from "@/components/PostFeatured";
 
+export const dynamic = "force-static";
 export default function HomePage() {
   return (
     <>
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,11 +1,10 @@
 import { SinglePost } from "@/components/SinglePost";
 import { SpinLoader } from "@/components/SpinLoader";
-import {
-  findAllPublicPostsCached,
-  findPostBySlugCached,
-} from "@/lib/post/queries";
+import { findPostBySlugCached } from "@/lib/post/queries";
 import { Metadata } from "next";
 import { Suspense } from "react";
+
+export const dynamic = "force-static";
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
